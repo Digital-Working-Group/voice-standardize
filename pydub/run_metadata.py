6425b4e7d1319b7afc04f9ea4ff3749d5abf64d5
Cody Karjadi <ckarjadi@fhs.org>
2025-03-18 16:37:21 -0400

    adjusted pydub/README.MD to display the examples in the given format (json/CSV/python). Added more comment information in pydub/run_metadata.py

--- pydub/run_metadata.py
+++ pydub/run_metadata.py
@@ -8,7 +8,7 @@ def main():
     """
     run metadata writing examples
     """
-    # All input files in ../sample_audio
+    ## All input files in ../sample_audio;
     flac_mono = '../sample_audio/flac/mono_first_ten_Sample_HV_Clip.flac'
     flac_stereo = '../sample_audio/flac/first_ten_Sample_HV_Clip.flac'
     mp3_mono = '../sample_audio/mp3/common_voice_en_21635524.mp3'
@@ -18,7 +18,7 @@ def main():
     wav_mono = '../sample_audio/wav/mono_first_ten_Sample_HV_Clip.wav'
     wav_stereo = '../sample_audio/wav/first_ten_Sample_HV_Clip.wav'
 
-    # Generated
+    ## Audio files that were generated via this module;
     kwargs = {'append_json_dict':{'ffmpeg_command': f"ffmpeg -i '{wav_mono}' -compression_level 5 -af aformat=s16:44100 '{flac_mono}'"}}
     write_metadata(flac_mono, **kwargs)
     kwargs = {'append_json_dict': {'ffmpeg_command': f"ffmpeg -i '{wav_stereo}' -compression_level 5 -af aformat=s16:44100 '{flac_stereo}'"}}
@@ -30,7 +30,7 @@ def main():
     kwargs = {'append_json_dict':{'ffmpeg_command': f"ffmpeg -i '{wav_stereo}' -ar 44100 -ac 1 '{wav_mono}'"}}
     write_metadata(wav_mono, **kwargs)
 
-    # Non-generated
+    ## Original audio files;
     write_metadata(wav_stereo)
     write_metadata(m4a_stereo)
     write_metadata(mp3_mono)
